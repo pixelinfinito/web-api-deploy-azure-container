bdb49ab47d7d003c91c9205f3221acb2927d1e2e
Paulo L. Estevão <paulo.estevao@pixel.ao>
2022-11-25 16:47:40 +0100

    feat: :sparkles: add crud

--- main.go
+++ main.go
@@ -1,7 +1,84 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"strconv"
+	"sync"
+
+	"github.com/labstack/echo/v4"
+)
+
+type (
+	user struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	}
+)
+
+var (
+	users = map[int]*user{}
+	seq   = 1
+	lock  = sync.Mutex{}
+)
 
 func main() {
-	fmt.Println("Starting")
+	e := echo.New()
+
+	api := e.Group("/api")
+	{
+		api.GET("/users", getAllUsers)
+		api.POST("/users", createUser)
+		api.GET("/users/:id", getUser)
+		api.PUT("/users/:id", updateUser)
+		api.DELETE("/users/:id", deleteUser)
+	}
+
+	e.Logger.Fatal(e.Start(":1234"))
+}
+
+func createUser(c echo.Context) error {
+	lock.Lock()
+	defer lock.Unlock()
+	u := &user{
+		ID: seq,
+	}
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	users[u.ID] = u
+	seq++
+	return c.JSON(http.StatusCreated, u)
+}
+
+func getUser(c echo.Context) error {
+	lock.Lock()
+	defer lock.Unlock()
+	id, _ := strconv.Atoi(c.Param("id"))
+	return c.JSON(http.StatusOK, users[id])
+}
+
+func updateUser(c echo.Context) error {
+	lock.Lock()
+	defer lock.Unlock()
+	u := new(user)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	id, _ := strconv.Atoi(c.Param("id"))
+	users[id].Name = u.Name
+	return c.JSON(http.StatusOK, users[id])
+}
+
+func deleteUser(c echo.Context) error {
+	lock.Lock()
+	defer lock.Unlock()
+	id, _ := strconv.Atoi(c.Param("id"))
+	delete(users, id)
+	return c.NoContent(http.StatusNoContent)
+}
+
+func getAllUsers(c echo.Context) error {
+	lock.Lock()
+	defer lock.Unlock()
+	return c.JSON(http.StatusOK, users)
 }
